6dc9f468d55e129f71a303e9cb52a7d7a046452e
Sofia Montana <smontana@knights.ucf.edu>
2021-02-08 00:05:51 -0500

    starting adding the card layout

--- js/script.js
+++ js/script.js
@@ -54,9 +54,49 @@ async function getContacts() {
     await makeContacts(contacts)
 }
 
+// async function makeContacts(contacts) {
+//     // getting out list element
+//     let ulEl = document.getElementById('contact-list');
+
+//     await console.log(contacts)
+//     // creating list item and giving it bootstrap class
+   
+   
+//     contacts.results.forEach(el => {
+//         console.log(el);
+
+//         let listEl = document.createElement("li");
+//         let aref = document.createElement('a');
+//         listEl.classList.add("list-group-item");
+
+//          // getting our inputs
+//         let fName = el.name;
+//         // let lName = document.getElementById('lastName').value;
+//         // let number = document.getElementById('phone-number').value;
+
+//         // adding our input t our list item
+        
+//         listEl.appendChild(document.createTextNode(fName));
+//         let updateBtn = document.createElement('button');
+//         updateBtn.textContent = "Update";
+//         updateBtn.setAttribute('class', "btn btn-secondary");
+//         updateBtn.setAttribute('data-bs-toggle', "modal");
+//         updateBtn.setAttribute('data-bs-target', "#myModal2");
+
+//         let deleteBtn = document.createElement('button');
+//         deleteBtn.textContent = "Delete";
+//         deleteBtn.setAttribute('class', "btn btn-secondary");
+
+//         listEl.appendChild(updateBtn);
+//         listEl.appendChild(deleteBtn);
+
+//         // adding our list item to our list
+//         ulEl.appendChild(listEl);
+//     });
+// }
 async function makeContacts(contacts) {
     // getting out list element
-    let ulEl = document.getElementById('contact-list');
+    let div = document.getElementById('card');
 
     await console.log(contacts)
     // creating list item and giving it bootstrap class
@@ -65,18 +105,28 @@ async function makeContacts(contacts) {
     contacts.results.forEach(el => {
         console.log(el);
 
-        let listEl = document.createElement("li");
-        let aref = document.createElement('a');
-        listEl.classList.add("list-group-item");
+        let divBody = document.createElement("div");
+        divBody.setAttribute('class', "card-body");
+
+        let cardimg = document.createElement('img');
+        cardimg.setAttribute('class', 'card-img-top');
+
+        let cardTitle = document.createElement('h5');
+        cardTitle.setAttribute('class', "card-title");
+
+        let cardText = document.createElement('h6');
+        cardText.setAttribute('class', 'card-text');
 
          // getting our inputs
         let fName = el.name;
+        cardimg.alt = 'Nothing'
         // let lName = document.getElementById('lastName').value;
         // let number = document.getElementById('phone-number').value;
 
         // adding our input t our list item
         
-        listEl.appendChild(document.createTextNode(fName));
+        cardTitle.appendChild(document.createTextNode(fName));
+        cardText.appendChild(document.createTextNode("random number :)"));
         let updateBtn = document.createElement('button');
         updateBtn.textContent = "Update";
         updateBtn.setAttribute('class', "btn btn-secondary");
@@ -86,12 +136,20 @@ async function makeContacts(contacts) {
         let deleteBtn = document.createElement('button');
         deleteBtn.textContent = "Delete";
         deleteBtn.setAttribute('class', "btn btn-secondary");
+        
+        let cardDiv = document.createElement("div");
+        cardDiv.setAttribute('class', "card");
+        cardDiv.style.width = '16rem';
 
-        listEl.appendChild(updateBtn);
-        listEl.appendChild(deleteBtn);
+        divBody.appendChild(cardTitle);
+        divBody.appendChild(cardText);
+        divBody.appendChild(updateBtn);
+        divBody.appendChild(deleteBtn);
 
         // adding our list item to our list
-        ulEl.appendChild(listEl);
+        cardDiv.appendChild(cardimg);
+        cardDiv.appendChild(divBody);
+        div.appendChild(cardDiv);
     });
 }
 
