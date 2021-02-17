e53b0e4ad1a4cbda43268367e3222cc9cbb9a613
guillermo-ggs <ggsparacino@gmail.com>
2021-02-17 17:25:19 -0500

    Updated search function

--- js/script.js
+++ js/script.js
@@ -510,8 +510,8 @@ function getUserID() {
     }
 
     // If we couldn't find the ID, redirect to login screen.
-    // if (userId < 0)
-    //     window.location.href = "login.html";
+    if (userId < 0)
+        window.location.href = "login.html";
 
     console.log("Fetched userId: " + userId);
     return userId;
