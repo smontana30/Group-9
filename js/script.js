bf4683fb0ee360eeedd8ea6c13d4f47923d15bf7
guillermo-ggs <ggsparacino@gmail.com>
2021-02-18 12:09:22 -0500

    Working on update function

--- js/script.js
+++ js/script.js
@@ -363,28 +363,28 @@ async function makeContacts(contacts) {
                             cardText.innerText = text;
                             let letter = updateFName.toLowerCase().charAt(0);
                             cardimg.src = "https://raw.githubusercontent.com/smontana30/Group-9/master/assets/letters/png/" + letter + ".png";
+                            console.log(payload);
+                            try {
+                                let payload = JSON.stringify({
+                                    'FirstName': updateFName,
+                                    'LastName': updateLName,
+                                    'Phone': updateNum,
+                                    'ID': updateId,
+                                    'UserID': getUserID()
+                                });
+                                let url = "http://tinytelephonetime.ninja/api/edit_contact.php";
+                                let xhr = new XMLHttpRequest();
+                                xhr.open("POST", url, false);
+                                xhr.setRequestHeader("Content-type", "application/json", "charset=UTF-8");
+                                xhr.send(payload);
+                            } catch (error) {
+                                // If we get here, there was likely an issue with the API.
+                                // document.getElementById("error-tag").innerHTML = err.message;
+                                console.error("Error:\n" + error)
+                            }
                         }
                     }
                 });
-
-                try {
-                    let payload = JSON.stringify({
-                        'FirstName': updateFName,
-                        'LastName': updateLName,
-                        'Phone': updateNum,
-                        'ID': updateId,
-                        'UserID': getUserID()
-                    });
-                    let url = "http://tinytelephonetime.ninja/api/edit_contact.php";
-                    let xhr = new XMLHttpRequest();
-                    xhr.open("POST", url, false);
-                    xhr.setRequestHeader("Content-type", "application/json", "charset=UTF-8");
-                    xhr.send(payload);
-                } catch (error) {
-                    // If we get here, there was likely an issue with the API.
-                    // document.getElementById("error-tag").innerHTML = err.message;
-                    console.error("Error:\n" + error)
-                }
 
                 document.getElementById('updateFname').value = "";
                 document.getElementById('updateLast').value = "";
