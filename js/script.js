6dbe655c4991413c552b31fd02e525684351f1cb
guillermo-ggs <ggsparacino@gmail.com>
2021-02-17 23:34:38 -0500

    Working on update function

--- js/script.js
+++ js/script.js
@@ -358,7 +358,7 @@ async function makeContacts(contacts) {
                         if (textContent.match(searchedCard)) {
                             let title = updateFName + " " + updateLName;
                             let text = updateNum;
-                            updateId = this.getAttribute('id');
+                            updateId = document.getElementById("div").id;
                             cardTitle.innerText = title;
                             cardText.innerText = text;
                             let letter = updateFName.toLowerCase().charAt(0);
