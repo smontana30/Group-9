d078a2fd8511fd6ac339e0d1c1530ca882b4d5da
Sofia Montana <smontana@knights.ucf.edu>
2021-02-24 11:48:54 -0500

    changed sign up redirect

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,4 @@
-function signup()
-{
+function signup() {
     const signup_form = document.getElementById("signup-form");
     const email = signup_form.email.value;
     const password = signup_form.password.value;
@@ -8,17 +7,16 @@ function signup()
     const lastName = signup_form.lastName.value;
     const url = "http://tinytelephonetime.ninja/api/add_user.php";
     const signup_obj = {
-        'firstName':firstName,
-        'lastName':lastName,
-        'login':email,
-        'password':password
+        'firstName': firstName,
+        'lastName': lastName,
+        'login': email,
+        'password': password
     }
     const payload = JSON.stringify(signup_obj);
     console.log("Payload: " + payload);
     var xhr = new XMLHttpRequest();
 
-    if (password.localeCompare(pw_confirm) != 0)
-    {
+    if (password.localeCompare(pw_confirm) != 0) {
         const msg = "Passwords don't match.";
         console.log(msg);
         document.getElementById("error-tag").innerHTML = msg;
@@ -28,20 +26,16 @@ function signup()
     xhr.open("POST", url, false);
     xhr.setRequestHeader("Content-type", "application/json", "charset=UTF-8");
 
-    try
-    {
+    try {
         xhr.send(payload);
         const response = JSON.parse(xhr.responseText);
-        if (response.error.localeCompare("") != 0)
-        {
+        if (response.error.localeCompare("") != 0) {
             document.getElementById("error-tag").innerHTML = response.error;
             console.log(response.error);
             return;
         }
-        window.location.href = "index.html";
-    }
-    catch (err)
-    {
+        window.location.href = "login.html";
+    } catch (err) {
         // If we get here, there was likely an issue with the API.
         document.getElementById("error-tag").innerHTML = err.message;
         console.error("Error:\n" + err);
